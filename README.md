79093372de0703c72dc42b2a7faa6fd8c14edab8
assemblygod <ijustwds@gmail.com>
2024-01-17 19:23:45 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Timetable IIT Madrass
+=====================
+
+
+Hosted [here](https://assemblygod.github.io/)
+
+Navigate IIT Madras' enigmatic slot labyrinth with this tool—a fleeting light in the shadowy realm of academic convolution.
---
+++ js/ics.js
@@ -0,0 +1,554 @@
+/* FileSaver.js
+ * A saveAs() FileSaver implementation.
+ * 2013-01-23
+ *
+ * By Eli Grey, http://eligrey.com
+ * License: X11/MIT
+ *   See LICENSE.md
+ */
+
+/*global self */
+/*jslint bitwise: true, regexp: true, confusion: true, es5: true, vars: true, white: true,
+  plusplus: true */
+
+/*! @source http://purl.eligrey.com/github/FileSaver.js/blob/master/FileSaver.js */
+
+var saveAs = saveAs
+  || (navigator.msSaveOrOpenBlob && navigator.msSaveOrOpenBlob.bind(navigator))
+  || (function(view) {
+	"use strict";
+	var
+		  doc = view.document
+		  // only get URL when necessary in case BlobBuilder.js hasn't overridden it yet
+		, get_URL = function() {
+			return view.URL || view.webkitURL || view;
+		}
+		, URL = view.URL || view.webkitURL || view
+		, save_link = doc.createElementNS("http://www.w3.org/1999/xhtml", "a")
+		, can_use_save_link =  !view.externalHost && "download" in save_link
+		, click = function(node) {
+			var event = doc.createEvent("MouseEvents");
+			event.initMouseEvent(
+				"click", true, false, view, 0, 0, 0, 0, 0
+				, false, false, false, false, 0, null
+			);
+			node.dispatchEvent(event);
+		}
+		, webkit_req_fs = view.webkitRequestFileSystem
+		, req_fs = view.requestFileSystem || webkit_req_fs || view.mozRequestFileSystem
+		, throw_outside = function (ex) {
+			(view.setImmediate || view.setTimeout)(function() {
+				throw ex;
+			}, 0);
+		}
+		, force_saveable_type = "application/octet-stream"
+		, fs_min_size = 0
+		, deletion_queue = []
+		, process_deletion_queue = function() {
+			var i = deletion_queue.length;
+			while (i--) {
+				var file = deletion_queue[i];
+				if (typeof file === "string") { // file is an object URL
+					URL.revokeObjectURL(file);
+				} else { // file is a File
+					file.remove();
+				}
+			}
+			deletion_queue.length = 0; // clear queue
+		}
+		, dispatch = function(filesaver, event_types, event) {
+			event_types = [].concat(event_types);
+			var i = event_types.length;
+			while (i--) {
+				var listener = filesaver["on" + event_types[i]];
+				if (typeof listener === "function") {
+					try {
+						listener.call(filesaver, event || filesaver);
+					} catch (ex) {
+						throw_outside(ex);
+					}
+				}
+			}
+		}
+		, FileSaver = function(blob, name) {
+			// First try a.download, then web filesystem, then object URLs
+			var
+				  filesaver = this
+				, type = blob.type
+				, blob_changed = false
+				, object_url
+				, target_view
+				, get_object_url = function() {
+					var object_url = get_URL().createObjectURL(blob);
+					deletion_queue.push(object_url);
+					return object_url;
+				}
+				, dispatch_all = function() {
+					dispatch(filesaver, "writestart progress write writeend".split(" "));
+				}
+				// on any filesys errors revert to saving with object URLs
+				, fs_error = function() {
+					// don't create more object URLs than needed
+					if (blob_changed || !object_url) {
+						object_url = get_object_url(blob);
+					}
+					if (target_view) {
+						target_view.location.href = object_url;
+					} else {
+                        window.open(object_url, "_blank");
+                    }
+					filesaver.readyState = filesaver.DONE;
+					dispatch_all();
+				}
+				, abortable = function(func) {
+					return function() {
+						if (filesaver.readyState !== filesaver.DONE) {
+							return func.apply(this, arguments);
+						}
+					};
+				}
+				, create_if_not_found = {create: true, exclusive: false}
+				, slice
+			;
+			filesaver.readyState = filesaver.INIT;
+			if (!name) {
+				name = "download";
+			}
+			if (can_use_save_link) {
+				object_url = get_object_url(blob);
+				save_link.href = object_url;
+				save_link.download = name;
+				click(save_link);
+				filesaver.readyState = filesaver.DONE;
+				dispatch_all();
+				return;
+			}
+			// Object and web filesystem URLs have a problem saving in Google Chrome when
+			// viewed in a tab, so I force save with application/octet-stream
+			// http://code.google.com/p/chromium/issues/detail?id=91158
+			if (view.chrome && type && type !== force_saveable_type) {
+				slice = blob.slice || blob.webkitSlice;
+				blob = slice.call(blob, 0, blob.size, force_saveable_type);
+				blob_changed = true;
+			}
+			// Since I can't be sure that the guessed media type will trigger a download
+			// in WebKit, I append .download to the filename.
+			// https://bugs.webkit.org/show_bug.cgi?id=65440
+			if (webkit_req_fs && name !== "download") {
+				name += ".download";
+			}
+			if (type === force_saveable_type || webkit_req_fs) {
+				target_view = view;
+			}
+			if (!req_fs) {
+				fs_error();
+				return;
+			}
+			fs_min_size += blob.size;
+			req_fs(view.TEMPORARY, fs_min_size, abortable(function(fs) {
+				fs.root.getDirectory("saved", create_if_not_found, abortable(function(dir) {
+					var save = function() {
+						dir.getFile(name, create_if_not_found, abortable(function(file) {
+							file.createWriter(abortable(function(writer) {
+								writer.onwriteend = function(event) {
+									target_view.location.href = file.toURL();
+									deletion_queue.push(file);
+									filesaver.readyState = filesaver.DONE;
+									dispatch(filesaver, "writeend", event);
+								};
+								writer.onerror = function() {
+									var error = writer.error;
+									if (error.code !== error.ABORT_ERR) {
+										fs_error();
+									}
+								};
+								"writestart progress write abort".split(" ").forEach(function(event) {
+									writer["on" + event] = filesaver["on" + event];
+								});
+								writer.write(blob);
+								filesaver.abort = function() {
+									writer.abort();
+									filesaver.readyState = filesaver.DONE;
+								};
+								filesaver.readyState = filesaver.WRITING;
+							}), fs_error);
+						}), fs_error);
+					};
+					dir.getFile(name, {create: false}, abortable(function(file) {
+						// delete file if it already exists
+						file.remove();
+						save();
+					}), abortable(function(ex) {
+						if (ex.code === ex.NOT_FOUND_ERR) {
+							save();
+						} else {
+							fs_error();
+						}
+					}));
+				}), fs_error);
+			}), fs_error);
+		}
+		, FS_proto = FileSaver.prototype
+		, saveAs = function(blob, name) {
+			return new FileSaver(blob, name);
+		}
+	;
+	FS_proto.abort = function() {
+		var filesaver = this;
+		filesaver.readyState = filesaver.DONE;
+		dispatch(filesaver, "abort");
+	};
+	FS_proto.readyState = FS_proto.INIT = 0;
+	FS_proto.WRITING = 1;
+	FS_proto.DONE = 2;
+
+	FS_proto.error =
+	FS_proto.onwritestart =
+	FS_proto.onprogress =
+	FS_proto.onwrite =
+	FS_proto.onabort =
+	FS_proto.onerror =
+	FS_proto.onwriteend =
+		null;
+
+	view.addEventListener("unload", process_deletion_queue, false);
+	return saveAs;
+}(self));
+
+
+/* global saveAs, Blob, BlobBuilder, console */
+/* exported ics */
+
+var ics = function(uidDomain, prodId) {
+  'use strict';
+
+  if (navigator.userAgent.indexOf('MSIE') > -1 && navigator.userAgent.indexOf('MSIE 10') == -1) {
+    console.log('Unsupported Browser');
+    return;
+  }
+
+  if (typeof uidDomain === 'undefined') { uidDomain = 'default'; }
+  if (typeof prodId === 'undefined') { prodId = 'Calendar'; }
+
+  var SEPARATOR = (navigator.appVersion.indexOf('Win') !== -1) ? '\r\n' : '\n';
+  var calendarEvents = [];
+  var calendarStart = [
+    'BEGIN:VCALENDAR',
+    'PRODID:' + prodId,
+    'VERSION:2.0'
+  ].join(SEPARATOR);
+  var calendarEnd = SEPARATOR + 'END:VCALENDAR';
+  var BYDAY_VALUES = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'];
+
+  return {
+    /**
+     * Returns events array
+     * @return {array} Events
+     */
+    'events': function() {
+      return calendarEvents;
+    },
+
+    /**
+     * Returns calendar
+     * @return {string} Calendar in iCalendar format
+     */
+    'calendar': function() {
+      return calendarStart + SEPARATOR + calendarEvents.join(SEPARATOR) + calendarEnd;
+    },
+
+    /**
+     * Add event to the calendar
+     * @param  {string} subject     Subject/Title of event
+     * @param  {string} location    Location of event
+     * @param  {string} begin       Beginning date of event
+     * @param  {string} stop        Ending date of event
+     */
+    'addEvent': function(subject, location, begin, stop, rrule) {
+      // I'm not in the mood to make these optional... So they are all required
+      if (typeof subject === 'undefined' ||
+        typeof begin === 'undefined' ||
+        typeof stop === 'undefined'
+      ) {
+        return false;
+      }
+
+      // validate rrule
+      if (rrule) {
+        if (!rrule.rrule) {
+          if (rrule.freq !== 'YEARLY' && rrule.freq !== 'MONTHLY' && rrule.freq !== 'WEEKLY' && rrule.freq !== 'DAILY') {
+            throw "Recurrence rrule frequency must be provided and be one of the following: 'YEARLY', 'MONTHLY', 'WEEKLY', or 'DAILY'";
+          }
+
+          if (rrule.until) {
+            if (isNaN(Date.parse(rrule.until))) {
+              throw "Recurrence rrule 'until' must be a valid date string";
+            }
+          }
+
+          if (rrule.interval) {
+            if (isNaN(parseInt(rrule.interval))) {
+              throw "Recurrence rrule 'interval' must be an integer";
+            }
+          }
+
+          if (rrule.count) {
+            if (isNaN(parseInt(rrule.count))) {
+              throw "Recurrence rrule 'count' must be an integer";
+            }
+          }
+
+          if (typeof rrule.byday !== 'undefined') {
+            if ((Object.prototype.toString.call(rrule.byday) !== '[object Array]')) {
+              throw "Recurrence rrule 'byday' must be an array";
+            }
+
+            if (rrule.byday.length > 7) {
+              throw "Recurrence rrule 'byday' array must not be longer than the 7 days in a week";
+            }
+
+            // Filter any possible repeats
+            rrule.byday = rrule.byday.filter(function(elem, pos) {
+              return rrule.byday.indexOf(elem) == pos;
+            });
+
+            for (var d in rrule.byday) {
+              if (BYDAY_VALUES.indexOf(rrule.byday[d]) < 0) {
+                throw "Recurrence rrule 'byday' values must include only the following: 'SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'";
+              }
+            }
+          }
+        }
+      }
+
+      //TODO add time and time zone? use moment to format?
+      var start_date = new Date(begin);
+      var end_date = new Date(stop);
+      var now_date = new Date();
+
+      var start_year = ("0000" + (start_date.getFullYear().toString())).slice(-4);
+      var start_month = ("00" + ((start_date.getMonth() + 1).toString())).slice(-2);
+      var start_day = ("00" + ((start_date.getDate()).toString())).slice(-2);
+      var start_hours = ("00" + (start_date.getHours().toString())).slice(-2);
+      var start_minutes = ("00" + (start_date.getMinutes().toString())).slice(-2);
+      var start_seconds = ("00" + (start_date.getSeconds().toString())).slice(-2);
+
+      var end_year = ("0000" + (end_date.getFullYear().toString())).slice(-4);
+      var end_month = ("00" + ((end_date.getMonth() + 1).toString())).slice(-2);
+      var end_day = ("00" + ((end_date.getDate()).toString())).slice(-2);
+      var end_hours = ("00" + (end_date.getHours().toString())).slice(-2);
+      var end_minutes = ("00" + (end_date.getMinutes().toString())).slice(-2);
+      var end_seconds = ("00" + (end_date.getSeconds().toString())).slice(-2);
+
+      var now_year = ("0000" + (now_date.getFullYear().toString())).slice(-4);
+      var now_month = ("00" + ((now_date.getMonth() + 1).toString())).slice(-2);
+      var now_day = ("00" + ((now_date.getDate()).toString())).slice(-2);
+      var now_hours = ("00" + (now_date.getHours().toString())).slice(-2);
+      var now_minutes = ("00" + (now_date.getMinutes().toString())).slice(-2);
+      var now_seconds = ("00" + (now_date.getSeconds().toString())).slice(-2);
+
+      // Since some calendars don't add 0 second events, we need to remove time if there is none...
+      var start_time = '';
+      var end_time = '';
+      if (start_hours + start_minutes + start_seconds + end_hours + end_minutes + end_seconds != 0) {
+        start_time = 'T' + start_hours + start_minutes + start_seconds;
+        end_time = 'T' + end_hours + end_minutes + end_seconds;
+      }
+      var now_time = 'T' + now_hours + now_minutes + now_seconds;
+
+      var start = start_year + start_month + start_day + start_time;
+      var end = end_year + end_month + end_day + end_time;
+      var now = now_year + now_month + now_day + now_time;
+
+      // recurrence rrule vars
+      var rruleString;
+      if (rrule) {
+        if (rrule.rrule) {
+          rruleString = rrule.rrule;
+        } else {
+          rruleString = 'rrule:FREQ=' + rrule.freq;
+
+          if (rrule.until) {
+            var uDate = new Date(Date.parse(rrule.until)).toISOString();
+            rruleString += ';UNTIL=' + uDate.substring(0, uDate.length - 13).replace(/[-]/g, '') + '000000Z';
+          }
+
+          if (rrule.interval) {
+            rruleString += ';INTERVAL=' + rrule.interval;
+          }
+
+          if (rrule.count) {
+            rruleString += ';COUNT=' + rrule.count;
+          }
+
+          if (rrule.byday && rrule.byday.length > 0) {
+            rruleString += ';BYDAY=' + rrule.byday.join(',');
+          }
+        }
+      }
+
+      var stamp = new Date().toISOString();
+
+      var calendarEvent = [
+        'BEGIN:VEVENT',
+        'UID:' + calendarEvents.length + "@" + uidDomain,
+        'CLASS:PUBLIC',
+        'DTSTAMP;VALUE=DATE-TIME:' + now,
+        'DTSTART;VALUE=DATE-TIME:' + start,
+        'DTEND;VALUE=DATE-TIME:' + end,
+        'LOCATION:' + location,
+        'SUMMARY;LANGUAGE=en-us:' + subject,
+        'TRANSP:TRANSPARENT',
+        'END:VEVENT'
+      ];
+
+      if (rruleString) {
+        calendarEvent.splice(4, 0, rruleString);
+      }
+
+      calendarEvent = calendarEvent.join(SEPARATOR);
+
+      calendarEvents.push(calendarEvent);
+      return calendarEvent;
+    },
+
+    /**
+     * Download calendar using the saveAs function from filesave.js
+     * @param  {string} filename Filename
+     * @param  {string} ext      Extention
+     */
+    'download': function(filename, ext) {
+      if (calendarEvents.length < 1) {
+        return false;
+      }
+
+      ext = (typeof ext !== 'undefined') ? ext : '.ics';
+      filename = (typeof filename !== 'undefined') ? filename : 'calendar';
+      var calendar = calendarStart + SEPARATOR + calendarEvents.join(SEPARATOR) + calendarEnd;
+
+      var blob;
+      if (navigator.userAgent.indexOf('MSIE 10') === -1) { // chrome or firefox
+        blob = new Blob([calendar]);
+      } else { // ie
+        var bb = new BlobBuilder();
+        bb.append(calendar);
+        blob = bb.getBlob('text/x-vCalendar;charset=' + document.characterSet);
+      }
+      saveAs(blob, filename + ext);
+      return calendar;
+    },
+
+    /**
+     * Build and return the ical contents
+     */
+    'build': function() {
+      if (calendarEvents.length < 1) {
+        return false;
+      }
+
+      var calendar = calendarStart + SEPARATOR + calendarEvents.join(SEPARATOR) + calendarEnd;
+
+      return calendar;
+    }
+  };
+};
+
+
+function downloadAsIcs() {
+  var startDateString = document.getElementById('startDate').value;
+  var endDateString = document.getElementById('endDate').value;
+
+  if (!startDateString || !endDateString) {
+      alert('Please enter both start date and end date.');
+      return;
+  }
+
+  var parseDate = function (dateString) {
+      var parts = dateString.split('-');
+      return new Date(parts[0], parts[1] - 1, parts[2]);
+  };
+
+  var startDate = parseDate(startDateString);
+  var endDate = parseDate(endDateString);
+
+  if (endDate < startDate) {
+      alert('End date must be equal to or after start date.');
+      return;
+  }
+
+  var cal = ics();
+
+  $('body').find('td.used').each(function () {
+      var venue = $(this).find('.course-venue').text();
+		  var course = $(this).find('.course-name').text();
+
+      var classes = $(this).attr('class').split(' ');
+
+      var day;
+      var time;
+
+      classes.forEach(function (className) {
+        if (className === 'f') {
+            time = '8:00-8:50';
+        } else if (className === 's') {
+            time = '9:00-9:50';
+        } else if (className === 'th') {
+            time = '10:00-10:50';
+        } else if (className === 'fo') {
+            time = '11:00-11:50';
+        } else if (className === 'fi') {
+            time = '13:00-13:50';
+        } else if (className === 'si') {
+            time = '14:00-16:45';
+        } else if (className === 'se') {
+            time = '17:00-17:50';
+        }
+      });
+
+      classes.forEach(function (className) {
+        if (className === 'mon') {
+            day = 'Mon';
+        } else if (className === 'tue') {
+            day = 'Tue';
+        } else if (className === 'wed') {
+            day = 'Wed';
+        } else if (className === 'thu') {
+            day = 'Thu'
+        } else if (className === 'fri') {
+            day = 'Fri'
+        }
+    
+      });
+
+      console.log(day, time)
+
+      if (day && time) {
+        var [startHour, startMinute, endHour, endMinute] = time.split(/[:-]/).map(part => parseInt(part));
+
+        var eventStartDate = new Date(startDate);
+        eventStartDate.setHours(startHour, startMinute, 0, 0);
+
+        var eventEndDate = new Date(startDate);
+        eventEndDate.setHours(endHour, endMinute, 0, 0);
+
+        var dayIndex = getDayIndex(day);
+        var daysDifference = (dayIndex - eventStartDate.getDay());
+        
+        if (daysDifference < 0)   daysDifference = (daysDifference + 7) % 7;
+          
+        eventStartDate.setDate(eventStartDate.getDate() + daysDifference);
+        eventEndDate.setDate(eventEndDate.getDate() + daysDifference);
+
+        console.log(eventStartDate, eventEndDate)
+
+        cal.addEvent(course, venue, eventStartDate, eventEndDate, { freq: 'WEEKLY', until: endDate });
+      }
+  });
+
+  cal.download();
+}
+
+function getDayIndex(day) {
+  return ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].indexOf(day);
+}
+
